679359fe1975336e6bc69796c9ca2537fffc15cf
Gokulavasan K R <65669031+GOKU-GO@users.noreply.github.com>
2022-10-11 09:42:43 +0530

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//no of digits using recursion
+// int noofdigits(int n){
+//     static int count = 0;
+//     if(n==0){
+//         return 0;
+//     }
+//     else{
+//         n = n/10;
+//         count++;
+//         noofdigits(n);
+//     }
+//     return count;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the digit:";
+//     cin>>n;
+//     cout<<noofdigits(n);
+//     return 0;
+    
+// }
+
